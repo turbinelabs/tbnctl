55a9699f06166649a7a21ffbbe89a93b4785fbcf
Glen D Sanford <9len@users.noreply.github.com>
2018-07-24 18:57:19 +0000

    prepare for release of OSS version 0.18.2 (#5764)
    fbshipit-source-id: 17ab349

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 	"github.com/turbinelabs/nonstdlib/log/console"
 )
 
-const TbnPublicVersion = "0.18.1"
+const TbnPublicVersion = "0.18.2"
 
 const clientApp = apiclient.App("github.com/turbinelabs/tbnctl")
 
